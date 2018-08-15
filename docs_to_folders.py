093b787bc7cbad9e4923004b6cade324d70a8f42
verdantfox <verdantfoxx@gmail.com>
2018-08-15 14:56:54 -0600

    Added --purge flag to docs_to_folders.py to allow purging folders before adding documents

--- docs_to_folders.py
+++ docs_to_folders.py
@@ -4,17 +4,18 @@ import click
 from lxml import etree
 import time
 import threading
+import sys
 
 
 with open('secure.txt', 'r') as f:
-	secure = dict()
-	for line in f:
-		if line != '' and line != '\n':
-			key, value = line.strip('\n').split(':')
-			secure[key] = value
+    secure = dict()
+    for line in f:
+        if line != '' and line != '\n':
+            key, value = line.strip('\n').split(':')
+            secure[key] = value
 
 options_string = '[-c <case names (comma separated if multiple)>] [-d <number of documents>] [-h <host name>]' \
-                 '[-p <port number>] [-u <user name>] [-w <password>]'
+                 '[-p <port number>] [-u <user name>] [-w <password>] [--purge]'
 
 
 @click.command(options_metavar=options_string)
@@ -25,100 +26,121 @@ options_string = '[-c <case names (comma separated if multiple)>] [-d <number of
 @click.option('-u', '--user-name', default=secure['username'], envvar='CARAMEL_USERNAME', metavar='<user name>')
 @click.option('-w', '--password', default=secure['password'], envvar='CARAMEL_PASSWORD', metavar='<password>')
 @click.option('-t', '--thread-count', default=10, metavar='<thread count>')
-def sample_folders(cases,  doc_count, host, port, user_name, password, thread_count):
-	"""Adds specified number of random documents from cases to folders using Caramel 'sample' method"""
-	auth = (user_name, password)
-	try:
-		doc_count = int(doc_count)
-		if doc_count <= 0:
-			raise click.BadParameter("doc_count must be an integer greater than 0")
-	except ValueError:
-		raise click.BadParameter("doc_count must be an integer greater than 0")
-	thread_count = int(thread_count)
-	if not 1 <= thread_count <= 100:
-		raise click.BadParameter("thread count must be a number between 1 and 100")
+@click.option('--purge', 'purge', is_flag=True)
+def sample_folders(cases,  doc_count, host, port, user_name, password, thread_count, purge):
+    """Adds specified number of random documents from cases to folders using Caramel 'sample' method"""
+    auth = (user_name, password)
+    try:
+        doc_count = int(doc_count)
+        if doc_count <= 0:
+            raise click.BadParameter("doc_count must be an integer greater than 0")
+    except ValueError:
+        raise click.BadParameter("doc_count must be an integer greater than 0")
+    thread_count = int(thread_count)
+    if not 1 <= thread_count <= 100:
+        raise click.BadParameter("thread count must be a number between 1 and 100")
 
-	cases = cases.strip(',').split(',')
-	click.echo("Host name: {}".format(host))
-	click.echo("Port number: {}".format(port))
-	click.echo("User name: {}".format(user_name))
-	click.echo("Plan to bring folders in case(s) to within 98% ({doc_98}) of {doc_count} random documents".format(
-		doc_98=int(doc_count * 0.98), doc_count=doc_count))
+    cases = cases.strip(',').split(',')
+    click.echo("Host name: {}".format(host))
+    click.echo("Port number: {}".format(port))
+    click.echo("User name: {}".format(user_name))
+    click.echo("Plan to bring folders in case(s) to within 98% ({doc_98}) of {doc_count} random documents".format(
+        doc_98=int(doc_count * 0.98), doc_count=doc_count))
 
-	for case in cases:
-		folder_feed = 'http://{host}:{port}/case/{case}/folder'.format(host=host, port=port, case=case)
-		try:
-			r = requests.get(folder_feed, auth=auth, params={'maxhits': '10000'})
-		except requests.exceptions.ConnectionError:
-			raise click.BadParameter("Host name not recognized!")
-		if r.status_code == 200:
-			pass  # Success
-		elif r.status_code == 404:
-			raise click.BadParameter("Case '{}' could not be found. Check case name's spelling.".format(case))
-		elif r.status_code == 401:
-			raise click.BadParameter("Unauthorized -- invalid login credentials. Check username and password.")
-		else:
-			raise click.BadParameter("Something went wrong. \nGot the following status code: '{code}'. "
-			                         "\nThe following headers were produced: '{headers}'".format(
-														code=r.status_code, headers=r.headers))
-		folder_count = len(etree.fromstring(r.text).findall('.//folder'))
-		click.echo("Working on '{case}': contains {folder_count} folders.".format(
-			case=case, folder_count=folder_count))
+    for case in cases:
+        folder_feed = 'http://{host}:{port}/case/{case}/folder'.format(host=host, port=port, case=case)
+        try:
+            r = requests.get(url=folder_feed, auth=auth, params={'maxhits': '10000'})
+        except requests.exceptions.ConnectionError:
+            raise click.BadParameter("Host name not recognized!")
+        if r.status_code == 200:
+            pass  # Success
+        elif r.status_code == 404:
+            raise click.BadParameter("Case '{}' could not be found. Check case name's spelling.".format(case))
+        elif r.status_code == 401:
+            raise click.BadParameter("Unauthorized -- invalid login credentials. Check username and password.")
+        else:
+            raise click.BadParameter("Something went wrong. \nGot the following status code: '{code}'. "
+                                     "\nThe following headers were produced: '{headers}'".format(
+                                                        code=r.status_code, headers=r.headers))
+        docs_url = 'http://{host}:{port}/case/{case}/document?'.format(
+            host=host, port=port, case=case)
+        docs_params = {'uhits': '1', 'maxhits': '0'}
+        document_request = requests.get(url=docs_url, auth=auth, params=docs_params)
+        case_doc_count = int(etree.fromstring(document_request.text).findall('.//unfiltered_hits')[0].text)
+        folder_count = len(etree.fromstring(r.text).findall('.//folder'))
+        click.echo("Working on '{case}':\n"
+                   "\tcontains {case_doc_count} documents\n"
+                   "\tcontains {folder_count} folders.".format(
+                        folder_count=folder_count, case=case, case_doc_count=case_doc_count))
 
-		max_threads = threading.BoundedSemaphore(thread_count)
+        if case_doc_count < doc_count:
+            sys.exit("Error:\tNot enough case documents to add to folders!\n"
+                     "\tNeed: {doc_count} case documents for your request\n"
+                     "\tHave: {case_doc_count} case documents\n\tExiting...".format(
+                            doc_count=doc_count, case_doc_count=case_doc_count))
 
-		with click.progressbar(etree.fromstring(r.text).findall('.//folder'),
-		                       label="Adding up to {} documents to each folder".format(doc_count)) as folders:
-			for folder in folders:
-				max_threads.acquire(blocking=True)
-				t = threading.Thread(target=add_docs,
-				                     args=(auth, host, port, case, doc_count, folder, max_threads))
-				t.start()
+        max_threads = threading.BoundedSemaphore(thread_count)
+
+        with click.progressbar(etree.fromstring(r.text).findall('.//folder'),
+                               label="Adding up to {} documents to each folder".format(doc_count)) as folders:
+            for folder in folders:
+                max_threads.acquire(blocking=True)
+                t = threading.Thread(target=add_docs,
+                                     args=(auth, host, port, case, doc_count, folder, max_threads, purge))
+                t.start()
 
 
-def add_docs(auth, host, port, case, doc_count, folder, max_threads):
-	"""Add specific number of documents to a folder in multi-threaded fashion"""
-	folder_id = folder.get('uri').split('/')[-1]
-	get_url = 'http://{host}:{port}/case/{case}/folder/{folder_id}/b'.format(
-		host=host, port=port, case=case, folder_id=folder_id)
+def add_docs(auth, host, port, case, doc_count, folder, max_threads, purge):
+    """Add specific number of documents to a folder in multi-threaded fashion"""
+    folder_id = folder.get('uri').split('/')[-1]
+    purge_url = 'http://{host}:{port}/case/{case}/folder/{folder_id}'.format(
+        host=host, port=port, case=case, folder_id=folder_id)
+    purge_headers = {'content-type': 'application/x-www-form-urlencoded'}
+    purge_data = {'_method': 'purge'}
+    if purge:
+        purge_request = requests.post(
+            url=purge_url, auth=auth, headers=purge_headers, data=purge_data)
 
-	while_passes = 0
-	get_attempts = 0
-	previous_doc_count = None
-	while True:
-		get_request = requests.get(url=get_url, auth=auth,
-		                           params={'facets': 'doc.id(count;limit=10000000)&maxhits=0'})
-		current_doc_count = int(etree.fromstring(get_request.text).findall('.//count')[0].text)
-		get_attempts += 1
-		# Sometimes takes a few seconds after POST for database update to show in GET
-		if while_passes != 0 and get_attempts < 11:
-			if previous_doc_count == current_doc_count:
-				time.sleep(1)
-				continue
-		if current_doc_count < doc_count:
-			docs_to_add = doc_count - current_doc_count
-		else:
-			break
-		# Escape valve for not having to exactly hit doc count.
-		# Sometimes sample will choose docs for folder that were already there.
-		if (current_doc_count / doc_count) > 0.98:
-			break
-		if docs_to_add > 0:
-			if docs_to_add / 10000 <= 1:
-				add_docs = docs_to_add
-			else:
-				add_docs = 10000
-			post_url = 'http://{host}:{port}/case/{case}/folder/{folder}'.format(
-				host=host, port=port, case=case, folder=folder_id)
-			headers = {'content-type': 'application/x-www-form-urlencoded'}
-			data = '_method=sample&target_count={add_docs}'.format(add_docs=add_docs)
-			post_request = requests.post(url=post_url, auth=auth, headers=headers, data=data)
-			previous_doc_count = current_doc_count
-		get_attempts = 0
-		while_passes += 1
-	max_threads.release()
-	return
+    get_url = 'http://{host}:{port}/case/{case}/folder/{folder_id}/b'.format(
+        host=host, port=port, case=case, folder_id=folder_id)
+    while_passes = 0
+    get_attempts = 0
+    previous_doc_count = None
+    while True:
+        get_request = requests.get(url=get_url, auth=auth,
+                                   params={'facets': 'doc.id(count;limit=10000000)&maxhits=0'})
+        current_doc_count = int(etree.fromstring(get_request.text).findall('.//count')[0].text)
+        get_attempts += 1
+        # Sometimes takes a few seconds after POST for database update to show in GET
+        if while_passes != 0 and get_attempts < 11:
+            if previous_doc_count == current_doc_count:
+                time.sleep(1)
+                continue
+        if current_doc_count < doc_count:
+            docs_to_add = doc_count - current_doc_count
+        else:
+            break
+        # Escape valve for not having to exactly hit doc count.
+        # Sometimes sample will choose docs for folder that were already there.
+        if (current_doc_count / doc_count) > 0.98:
+            break
+        if docs_to_add > 0:
+            if docs_to_add / 10000 <= 1:
+                add_docs = docs_to_add
+            else:
+                add_docs = 10000
+            post_url = 'http://{host}:{port}/case/{case}/folder/{folder}'.format(
+                host=host, port=port, case=case, folder=folder_id)
+            headers = {'content-type': 'application/x-www-form-urlencoded'}
+            data = '_method=sample&target_count={add_docs}'.format(add_docs=add_docs)
+            post_request = requests.post(url=post_url, auth=auth, headers=headers, data=data)
+            previous_doc_count = current_doc_count
+        get_attempts = 0
+        while_passes += 1
+    max_threads.release()
+    return
 
 
 if __name__ == '__main__':
-	sample_folders()
+    sample_folders()
